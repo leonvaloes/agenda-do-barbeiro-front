86619d41bb02524832cc8f463e3bf53ce7dfaff6
Leonardo Valões <leovaloesnovaes@gmail.com>
2025-04-22 17:03:57 -0300

    first commit

---
+++ src/components/Table/table.jsx
@@ -0,0 +1,64 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+
+import React, { useState } from "react";
+
+function Tables({
+    data,
+    columns,
+    setSelectedLine,
+}) {
+    const isListValid = Array.isArray(data) && data.length > 0;
+    return (
+        <>
+            <Table className="w-full border-spacing-y-2">
+                {isListValid ? (
+                    <>
+                        <TableHeader>
+                            <TableRow>
+                                {columns.map((column) => (
+                                    <TableCell
+                                        key={`${column.headerName}_${column.field}`}
+                                        className="px-4 py-2 font-bold text-center align-middle"
+                                    >
+                                        {column.headerName}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {data.map((row, rowIndex) => (
+                                <TableRow onClick={()=>setSelectedLine ? setSelectedLine(row) : "" } key={rowIndex} className="text-center align-middle" >
+                                    {columns.map((col) => (
+                                        <TableCell key={`${col.headerName}_${col.field}`} className="px-4 py-2 align-middle">
+                                            {col.renderCell
+                                                ? col.renderCell({ row })
+                                                : row[col.field]
+                                            }
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </>
+                ) : (
+                    <TableBody>
+                        <TableRow>
+                            <TableCell colSpan={columns.length} className="text-center py-4">
+                                Nenhum dado disponível
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                )}
+            </Table>
+        </>
+
+    );
+}
+
+export default Tables;
